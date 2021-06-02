c9c1c0a4faf105f0f7870241a07458a8967129be
Jay Bhiyani <jaybhiyani15@gmail.com>
2021-06-02 18:49:54 +0530

    Add Discount.Grpc

---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Discount.Grpc.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> DeleteDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+    }
+}
